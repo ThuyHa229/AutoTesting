d8f41d2100df7d747b139d01760943f62d889443
thuyha <thuyha@pnvl157.pn.com.vn>
2023-11-03 17:22:46 +0700

    rename

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -16,16 +16,40 @@ public class MainTest {
         webDriver.navigate().to("https://www.guru99.com");
         webDriver.manage().window().maximize();
 
-        String[] names = {"JUnit", "Agile Testing", "Quality Center(ALM)"};
-        String[] titles = {
+        String[] subMenuNames = {"JUnit", "Agile Testing", "Quality Center(ALM)","Bugzilla",
+                "HP Loadrunner","RPA","Cucumber","Software Testing",
+                "SAP Testing","Database Testing","Mobile Testing",
+                "Selenium","ETL Testing","Mantis",
+                "SoapUI","JMeter","Postman",
+                "TEST Management","JIRA","QTP","TestLink"
+        };
+        String[] subMenuTitles = {
                 "JUnit Tutorial for Beginners: Learn in 3 Days",
                 "Agile Methodology Tutorial – Guru99",
-                "HP ALM /Quality Center Tutorial"
+                "HP ALM /Quality Center Tutorial",
+                "Bugzilla Tutorial – Defect Tracking Tool",
+                "HP Loadrunner Tutorial for Beginners",
+                "RPA Tutorial for Beginners: Learn Robotic Process Automation",
+                "Cucumber Testing Tutorials for Beginners",
+                "Software Testing Tutorial",
+                "SAP Testing – Tools, Scenarios & Test Case Example",
+                "Database (Data) Testing Tutorial",
+                "Free Mobile App Testing Tutorial",
+                "Selenium Tutorial",
+                "ETL Testing Tutorial",
+                "MANTIS Bug Tracker Tutorial",
+                "SoapUI Tutorials for Beginners",
+                "JMeter Tutorial for Beginners: Learn in 7 Days",
+                "Postman Tutorial – How to use for API Testing?",
+                "TEST Management Tutorials: Complete Training Course",
+                "What is JIRA Testing Tool? Tutorial for Beginners",
+                "UFT/QTP Tutorial for Beginners: Learn in 7 Days",
+                "TestLink Tutorial: A Complete Guide"
                 };
-        int randomIndex = new Random().nextInt(names.length);
+        int randomIndex = new Random().nextInt(subMenuNames.length);
 
-        String randomName = names[randomIndex];
-        String randomTitle = titles[randomIndex];
+        String randomName = subMenuNames[randomIndex];
+        String randomTitle = subMenuTitles[randomIndex];
 
         //Xpath
         String menuHeaderXpathByText = "//ul[@id = 'primary-menu']//span[text() = '%s']";
@@ -42,12 +66,12 @@ public class MainTest {
         WebElement subMenuHeaderElement = webDriver.findElement(submenuHeaderByText);
 
         //Actions
-        menuHeaderElement.click();//click vào TEsting
+        menuHeaderElement.click();//click vào submenu
 
         WebDriverWait wait = new WebDriverWait(webDriver,1);
         wait.until(ExpectedConditions.visibilityOf(subMenuHeaderElement));
 
-        subMenuHeaderElement.click();// click vào JUnit
+        subMenuHeaderElement.click();// click vào title
 
         WebElement postTileElement = webDriver.findElement(postTitle);
         String actualPostTitle = postTileElement.getText();
